32bc53c731bf214f24dc7e8047b1b7411136a2f1
bibar228 <profik0077@mail.ru>
2024-05-06 16:30:37 +0500

    v. 2.0

--- tiun/tiun.py
+++ tiun/tiun.py
@@ -9,27 +9,28 @@ wookbook = openpyxl.load_workbook("Список заявок СК Репка.xls
 ws = wookbook.active
 
 for row in range(2, ws.max_row):
-    # print("ФИО1", ws.cell(row, 3).value)
-    # print("ФИОм", f"{ws.cell(row, 2).value.split()[0]} {ws.cell(row, 2).value.split()[1][0]}. {ws.cell(row, 2).value.split()[2][0]}.")
-    # print("АДРЕС1", ws.cell(row, 4).value)
-    # print("Телефон1", ws.cell(row, 5).value)
-    # print("Напряжение", str(ws.cell(row, 6).value).replace(".", ","))
-    # print("Мощность", str(ws.cell(row, 7).value).replace(".", ","))
-    # print("ПАСПОРТ1", ws.cell(row, 8).value)
-    # print("ТУ1", ws.cell(row, 9).value)
-    # print("НОМЕР1", ws.cell(row, 10).value)
-    # print("СУММА1", ws.cell(row, 12).value)
-    # print("АДПУ1", ws.cell(row, 13).value)
-    # print("ДАТА1", f"{strftime('%d %m %Y', gmtime()).split()[0]} {months[strftime('%d %m %Y', gmtime()).split()[1]]} {strftime('%d %m %Y', gmtime()).split()[2]}")
+    print("ФИО1", ws.cell(row, 3).value)
+    print("ФИОм", f"{ws.cell(row, 2).value.split()[0]} {ws.cell(row, 2).value.split()[1][0]}. {ws.cell(row, 2).value.split()[2][0]}.")
+    print("АДРЕС1", ws.cell(row, 4).value)
+    print("Телефон1", ws.cell(row, 5).value)
+    print("Напряжение", str(ws.cell(row, 6).value).replace(".", ","))
+    print("Мощность", str(ws.cell(row, 7).value).replace(".", ","))
+    print("ПАСПОРТ1", ws.cell(row, 8).value)
+    print("ТУ1", " ".join(ws.cell(row, 9).value.split()))
+    print("НОМЕР1", ws.cell(row, 10).value)
+    print("СУММА1", ws.cell(row, 12).value)
+    print("АДПУ1", ws.cell(row, 13).value)
+    print("ДАТА1", f"{strftime('%d %m %Y', gmtime()).split()[0]} {months[strftime('%d %m %Y', gmtime()).split()[1]]} {strftime('%d %m %Y', gmtime()).split()[2]}")
     doc = DocxTemplate("Уведомление типовое.docx")
     context = {'ФИО1': ws.cell(row, 3).value,
+               'ФИО3': ws.cell(row, 2).value,
                "ФИО2": f"{ws.cell(row, 2).value.split()[0]} {ws.cell(row, 2).value.split()[1][0]}. {ws.cell(row, 2).value.split()[2][0]}.",
                "АДРЕС1": ws.cell(row, 4).value,
                "Телефон1": ws.cell(row, 5).value,
                "Напряжение": str(ws.cell(row, 6).value).replace(".", ","),
                "Мощность": str(ws.cell(row, 7).value).replace(".", ","),
                "ПАСПОРТ1": ws.cell(row, 8).value,
-               "ТУ1": ws.cell(row, 9).value,
+               "ТУ1": " ".join(ws.cell(row, 9).value.split()),
                "НОМЕР1": ws.cell(row, 10).value,
                "СУММА1": ws.cell(row, 12).value,
                "АДПУ1": ws.cell(row, 13).value,
